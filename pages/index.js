2ad13eaf9ced14de6b131fe2f15a6972ae79f516
utkarshg6 <utkarshabhinav.torres@gmail.com>
2021-04-30 12:48:57 +0100

    Init Commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class ComponentIndex extends Component {
+
+    render() {
+        return (
+            <h3>Hello, Welcome to Carbon Cost Visualizer!</h3>
+        )
+    }
+}
+
+export default ComponentIndex
