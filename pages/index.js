8cc2093b1e200e285ccf623f3d133b19824ebeab
utkarshg6 <utkarshabhinav.torres@gmail.com>
2021-05-01 13:23:31 +0100

    Add Energy Consumption Bar Chart

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import Layout from '../components/layout';
+import Chart from "react-google-charts";
+
 class ComponentIndex extends Component {
+
+    renderBarChart() {
+        return (
+            <Chart
+                width={'800px'}
+                height={'300px'}
+                chartType="BarChart"
+                loader={<div>Loading Chart</div>}
+                data={[
+                    ['Blockchain', 'Energy Consumption (TWH)'],
+                    ['Ethereum', 33],
+                    ['RSK', 3]
+                ]}
+                options={{
+                    title: "Blockchain's Energy Consumption",
+                    chartArea: { width: '50%' },
+                    hAxis: {
+                        title: 'Total Consumption',
+                        minValue: 0,
+                    },
+                    vAxis: {
+                        title: 'Blockchain',
+                    },
+                }}
+                // For tests
+                rootProps={{ 'data-testid': '1' }}
+            />
+        )
+    }
 
     render() {
         return (
             <Layout>
-                <h3>Enjoy!</h3>
+                {this.renderBarChart()}
             </Layout>
         )
     }
