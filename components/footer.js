050d7d140e417029c5e66aa801d71853412e6b5c
utkarshg6 <utkarshabhinav.torres@gmail.com>
2021-05-02 08:34:55 +0100

    Add Footer

---
+++ components/footer.js
@@ -0,0 +1,21 @@
+/*
+The Footer file that will remain constant.
+*/
+
+import React, { Component } from 'react';
+import { Segment } from 'semantic-ui-react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <Segment
+                style={{ position: 'fixed', bottom: '0', width: '100%', borderRadius: '0', textAlign: 'center' }}
+                inverted
+            >
+                Copyright Reserved by the Blockchain Carbon Cost Visualizer. This software and code was released under the Apache 2 license.
+            </Segment>
+        )
+    }
+}
+
+export default Footer
