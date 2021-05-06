7efe8f2fb0f98aedff1cf4417d1301b8af316982
utkarshg6 <utkarshabhinav.torres@gmail.com>
2021-05-06 13:36:05 +0100

    Add Styling to About Us Section

--- components/about-us-section.js
+++ components/about-us-section.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react'
-import { Container, Segment, Header, Card, Image } from 'semantic-ui-react';
+import { Segment, Header, Card, Image } from 'semantic-ui-react';
+
+const nameStyle = {
+    marginTop: '19px',
+    fontSize: '18px'
+}
 
 class AboutUs extends Component {
     render() {
         return (
             <Segment
-                style={{ width: '800px', height: '500px', color: 'black' }}
+                style={{ width: '800px', height: '600px', color: 'black' }}
             >
-                <Header as='h2'>
+                <Header
+                    style={{
+                        marginTop: '20px',
+                        fontSize: '30px'
+                    }}
+                    as='h2'
+                >
                     About Us
                 </Header>
-                <p>
+                <p
+                    style={{
+                        fontSize: '16px'
+                    }}
+                >
                     This is a project submitted by the team O.N.C.E (Operation Nifty Carbon Emissions).
                      <br></br>
                     This project is submitted under the Awareness track of the Green NFT Hackathon.
                 </p>
 
-                <Header as='h3'>
+                <Header
+                    style={{
+                        marginTop: '20px',
+                        fontSize: '30px'
+                    }}
+                    as='h2'
+                >
                     Team Members
                 </Header>
 
@@ -32,8 +53,11 @@ class AboutUs extends Component {
                                 size='large'
                                 src='/img/Brendan.png'
                             />
-                            <Card.Header>Brendan Graetz</Card.Header>
-                            <Card.Meta>Head of Developer Experience, IOV Labs</Card.Meta>
+                            <Card.Header
+                                style={nameStyle}
+                            >Brendan Graetz</Card.Header>
+                            <Card.Meta
+                            >Head of Developer Experience, IOV Labs</Card.Meta>
                         </Card.Content>
                     </Card>
 
@@ -48,7 +72,11 @@ class AboutUs extends Component {
                                 size='large'
                                 src='/img/Gaurang.jfif'
                             />
-                            <Card.Header>Gaurang Torvekar</Card.Header>
+                            <Card.Header
+                                style={nameStyle}
+                            >
+                                Gaurang Torvekar
+                                </Card.Header>
                             <Card.Meta>Co-founder and CEO at Indorse</Card.Meta>
                         </Card.Content>
                     </Card>
@@ -64,7 +92,11 @@ class AboutUs extends Component {
                                 size='large'
                                 src='/img/Vijay.png'
                             />
-                            <Card.Header>Vijay Krishnavanshi</Card.Header>
+                            <Card.Header
+                                style={nameStyle}
+                            >
+                                Vijay Krishnavanshi
+                                </Card.Header>
                             <Card.Meta>Senior Backend Developer at Indorse</Card.Meta>
                         </Card.Content>
                     </Card>
@@ -80,7 +112,11 @@ class AboutUs extends Component {
                                 size='large'
                                 src='/img/Utkarsh.jfif'
                             />
-                            <Card.Header>Utkarsh Gupta</Card.Header>
+                            <Card.Header
+                                style={nameStyle}
+                            >
+                                Utkarsh Gupta
+                                </Card.Header>
                             <Card.Meta>DAPP Developer at Indorse</Card.Meta>
                         </Card.Content>
                     </Card>
