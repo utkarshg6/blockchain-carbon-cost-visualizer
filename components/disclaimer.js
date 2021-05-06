303f595d0d3cfe0b979bcaecaeb7571336497797
utkarshg6 <utkarshabhinav.torres@gmail.com>
2021-05-06 13:21:36 +0100

    Add Styling to Disclaimer

--- components/disclaimer.js
+++ components/disclaimer.js
@@ -6,12 +6,22 @@ class WhatsThisSection extends Component {
     render() {
         return (
             <Segment
-                style={{ width: '500px', height: '200px', color: 'black' }}
+                style={{ width: '700px', height: '250px', color: 'black' }}
             >
-                <Header as='h2'>
+                <Header
+                    style={{
+                        marginTop: '10px',
+                        fontSize: '30px'
+                    }}
+                    as='h2'
+                >
                     Disclaimer
                 </Header>
-                <p>
+                <p style={{
+                    fontSize: '16px',
+                    marginLeft: '50px',
+                    marginRight: '50px'
+                }}>
                     While we have tried to use our best judgement and the most reliable sources of information available,
                     the values shown on this website are still approximations! We are attempting to show the relative carbon
                     emissions from various comparable transactions for one Smart Contract,
