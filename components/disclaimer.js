e4a12bf7d654dab61473a7642ff743bdcb9920a6
Gaurang Torvekar <gaurangtorvekar@gmail.com>
2021-05-07 14:51:28 +0100

    Small copy changes

--- components/disclaimer.js
+++ components/disclaimer.js
@@ -1,36 +1,32 @@
-import React, { Component } from 'react'
-import { Segment, Header } from 'semantic-ui-react';
-
+import React, { Component } from "react";
+import { Segment, Header } from "semantic-ui-react";
 
 class WhatsThisSection extends Component {
-    render() {
-        return (
-            <Segment
-                style={{ width: '700px', height: '250px', color: 'black' }}
-            >
-                <Header
-                    style={{
-                        marginTop: '10px',
-                        fontSize: '30px'
-                    }}
-                    as='h2'
-                >
-                    Disclaimer
-                </Header>
-                <p style={{
-                    fontSize: '16px',
-                    marginLeft: '50px',
-                    marginRight: '50px'
-                }}>
-                    While we have tried to use our best judgement and the most reliable sources of information available,
-                    the values shown on this website are still approximations! We are attempting to show the relative carbon
-                    emissions from various comparable transactions for one Smart Contract,
-                    and these values may vary based on the individual implementations of other contracts.
-                    We assume no liability for the accuracy of these values whatsoever.
-                </p>
-            </Segment>
-        )
-    }
+	render() {
+		return (
+			<Segment style={{ width: "700px", height: "250px", color: "black" }}>
+				<Header
+					style={{
+						marginTop: "10px",
+						fontSize: "30px",
+					}}
+					as="h2"
+				>
+					Disclaimer
+				</Header>
+				<p
+					style={{
+						fontSize: "16px",
+						marginLeft: "50px",
+						marginRight: "50px",
+					}}
+				>
+					While we have used our best judgement and the most reliable sources of information available, the values shown on this website are still approximations! Our intent is to show the relative carbon emissions from various comparable transactions for selected Smart Contracts, and these values
+					may vary for other Smart Contracts. We assume no liability for the accuracy of these values whatsoever.
+				</p>
+			</Segment>
+		);
+	}
 }
 
-export default WhatsThisSection
+export default WhatsThisSection;
--- components/footer.js
+++ components/footer.js
@@ -2,50 +2,54 @@
 The Footer file that will remain constant.
 */
 
-import React, { Component } from 'react';
-import { Menu, Header } from 'semantic-ui-react';
+import React, { Component } from "react";
+import { Menu, Header } from "semantic-ui-react";
 
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faCopyright } from '@fortawesome/free-solid-svg-icons';
 
 class Footer extends Component {
-    render() {
-        return (
-            <Menu
-                text
-                style={{
-                    position: 'fixed', bottom: '0', width: '100%', borderRadius: '0', textAlign: 'center',
-                    justifyContent: 'center',
-                    boxShadow: '0 0 50px 0 rgba(76,147,230,0.1)',
-                    fontSize: '16px',
-                    zIndex: '1',
-                    backgroundColor: 'white',
-                    marginBottom: '0px',
-                    height: '60px'
-                }}
-            // inverted
-            >
-                <Menu.Item>
-                    <Header
-                        style={{
-                            fontSize: '16px',
-                            lineHeight: '20px',
-                            marginLeft: '80px'
-                        }}
-                    >
-                        {/* <FontAwesomeIcon
+	render() {
+		return (
+			<Menu
+				text
+				style={{
+					position: "fixed",
+					bottom: "0",
+					width: "100%",
+					borderRadius: "0",
+					textAlign: "center",
+					justifyContent: "center",
+					boxShadow: "0 0 50px 0 rgba(76,147,230,0.1)",
+					fontSize: "16px",
+					zIndex: "1",
+					backgroundColor: "white",
+					marginBottom: "0px",
+					height: "60px",
+				}}
+				// inverted
+			>
+				<Menu.Item>
+					<Header
+						style={{
+							fontSize: "16px",
+							lineHeight: "20px",
+							marginLeft: "80px",
+						}}
+					>
+						{/* <FontAwesomeIcon
                             style={{
                                 height: '14px'
                             }}
                             icon={faCopyright}
                         />
                         {' '} */}
-                        Copyright Reserved by O.N.C.E project team. This software and code is released under the GPL v3 license.
-                    </Header>
-                </Menu.Item>
-            </Menu>
-        )
-    }
+						&copy; Reserved by O.N.C.E project team. This software and code is released under the GPL v3 license.
+					</Header>
+				</Menu.Item>
+			</Menu>
+		);
+	}
 }
 
-export default Footer
+export default Footer;
