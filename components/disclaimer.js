19983dc0f5422c54213c77101922b7da871371d8
utkarshg6 <utkarshabhinav.torres@gmail.com>
2021-05-07 15:18:32 +0100

    Update Disclaimer

--- components/disclaimer.js
+++ components/disclaimer.js
@@ -22,11 +22,10 @@ class WhatsThisSection extends Component {
                     marginLeft: '50px',
                     marginRight: '50px'
                 }}>
-                    While we have tried to use our best judgement and the most reliable sources of information available,
-                    the values shown on this website are still approximations! We are attempting to show the relative carbon
-                    emissions from various comparable transactions for one Smart Contract,
-                    and these values may vary based on the individual implementations of other contracts.
-                    We assume no liability for the accuracy of these values whatsoever.
+                    While we have used our best judgement and the most reliable sources of information available,
+                    the values shown on this website are still approximations!
+                    Our intent is to show the relative carbon emissions from various comparable transactions for selected Smart Contracts,
+                    and these values may vary for other Smart Contracts. We assume no liability for the accuracy of these values whatsoever.
                 </p>
             </Segment>
         )
